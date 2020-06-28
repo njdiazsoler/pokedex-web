fc34afe1541785bd74dd823589733150406a8b79
njdiazsoler <njdiaz90@gmail.com>
2020-06-28 20:48:29 -0300

    added bootstrap files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import "nes.css/css/nes.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

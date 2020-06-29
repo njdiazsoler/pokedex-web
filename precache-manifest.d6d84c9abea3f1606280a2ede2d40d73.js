ccb7756988305958f7b1fd502448ba25b346422d
njdiazsoler <njdiaz90@gmail.com>
2020-06-29 03:08:10 -0300

    Updates

---
+++ precache-manifest.d6d84c9abea3f1606280a2ede2d40d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e53a3209dde3654c92ef541c493b819",
+    "url": "/pokedex-web/index.html"
+  },
+  {
+    "revision": "e6ceea208ae1922e8c4e",
+    "url": "/pokedex-web/static/css/2.c3e47d7a.chunk.css"
+  },
+  {
+    "revision": "c01c6d959f45a0ec0b2a",
+    "url": "/pokedex-web/static/css/main.2b3d6161.chunk.css"
+  },
+  {
+    "revision": "e6ceea208ae1922e8c4e",
+    "url": "/pokedex-web/static/js/2.9a2181b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pokedex-web/static/js/2.9a2181b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01c6d959f45a0ec0b2a",
+    "url": "/pokedex-web/static/js/main.9c1db994.chunk.js"
+  },
+  {
+    "revision": "0418d1466a93da423e33",
+    "url": "/pokedex-web/static/js/runtime-main.61b3a2ba.js"
+  }
+]);

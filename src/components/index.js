729a2b2fcbdde20a036793a13e77c6f9e9c9078d
njdiazsoler <njdiaz90@gmail.com>
2020-06-29 01:47:22 -0300

    added components to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as CustomAlert } from './CustomAlert';
-export { default as TextInput } from './TextInput';
+export { default as CustomCard } from './CustomCard';
+export { default as PageFooter } from './PageFooter';
+export { default as TextInputWithButton } from './TextInputWithButton';

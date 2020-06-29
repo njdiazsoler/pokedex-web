55c9a40079c98e022675044767ebb1d388859f2b
njdiazsoler <njdiaz90@gmail.com>
2020-06-29 03:02:57 -0300

    added classes to card

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 
 const CustomCard = props => {
   const { name } = props.cardData;
-  return(<Card>
+  return(<Card className='pokedex-card px-1 py-2'>
     <p>{`${name.charAt(0).toUpperCase()}${name.slice(1)}`}</p>
   </Card>)
 }

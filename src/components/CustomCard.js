d93c259d4ddfeafc366c94be8b5c7a7ce3e68c52
njdiazsoler <njdiaz90@gmail.com>
2020-07-01 02:33:13 -0300

    added new elements and variables for custom card

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Image, Spinner } from 'react-bootstrap';
+import UIUtils from '../utils/ui';
 
-const CustomCard = props => {
-  const { name } = props.cardData;
-  return(<Card className='pokedex-card px-1 py-2'>
-    <p>{`${name.charAt(0).toUpperCase()}${name.slice(1)}`}</p>
-  </Card>)
-}
+const CustomCard = (props) => {
+  const { cardData } = props;
+  const { name, sprites, types } = cardData;
+  return (
+    <Card className="pokedex-card px-1 py-2 align-item-center">
+      <p>{`${UIUtils.capitalizeString(name)}`}</p>
+      <div className='d-flex flex-row'>
+        {sprites ? <Image rounded style={{ height: 100, width: 100 }} src={sprites && sprites.front_default} /> : <Spinner animation="border" />}
+        <div>{types && types.map(type => <p key={type.type.name}>{UIUtils.capitalizeString(type.type.name)}</p>)}</div>
+      </div>
+    </Card>
+  );
+};
 
 export default CustomCard;

d3b3da8afd006841abfe005ebd84e9db4ee2a9e9
njdiazsoler <njdiaz90@gmail.com>
2020-07-02 03:33:05 -0300

    added conditions for query options

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import config from '../config';
 
 const makeApiCall = async (path, options) => {
   if(options){
-    path += `?limit=${options.limit}&offset=${options.offset}`
+    path += `?limit=${options.limit ? options.limit : 0}&offset=${options.offset ? options.offset : 0}&keyword=${options.keyword ? options.keyword : 0}`
   }
   const promise = await axios.get(`${config.api.url}${path}`);
   return promise.data.data;
